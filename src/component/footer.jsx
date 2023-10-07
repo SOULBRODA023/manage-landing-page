5b534c726c89f958076c777f384d6f3dd746a789
Soulbroda23 <ogundeyi.dieko13@gmail.com>
2023-10-07 06:52:56 -0600

    done with the footer

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -13,12 +13,89 @@ const Footer = () => {
       <section className="footer__secondlayer">
         <section className="input">
           <form>
-            <input type="email" className="input__email" />
+            <input
+              type="email"
+              className="input__email"
+              placeholder="Updates in your inbox..."
+            />
             <button type="submit">
               <p>GO</p>
             </button>
           </form>
         </section>
+        <section className="links">
+          <ul>
+            <a href="">
+              <li>Home</li>
+            </a>
+            <a href="">
+              <li>Pricing</li>
+            </a>
+
+            <a href="">
+              <li>Products</li>
+            </a>
+            <a href="">
+              <li>About Us</li>
+            </a>
+          </ul>
+          <ul>
+            <a href="">
+              <li>Careers</li>
+            </a>
+            <a href="">
+              <li>Community </li>
+            </a>
+            <a href="">
+              <li>Privacy Policy</li>
+            </a>
+          </ul>
+        </section>
+        <section className="socials">
+          <a href="">
+            <img
+              src="./images/icon-facebook.svg"
+              alt="Facebook link"
+              className="social__links"
+            />
+          </a>
+          <a href="">
+            <img
+              src="./images/icon-youtube.svg"
+              alt="youtube link"
+              className="social__links"
+            />
+          </a>
+          <a href="">
+            <img
+              src="./images/icon-twitter.svg"
+              alt="Twitter link"
+              className="social__links"
+            />
+          </a>
+          <a href="">
+            <img
+              src="./images/icon-pinterest.svg"
+              alt="Pinterest link"
+              className="social__links"
+            />
+          </a>
+          <a href="">
+            <img
+              src="./images/icon-instagram.svg"
+              alt="Instagram link"
+              className="social__links"
+            />
+          </a>
+        </section>
+        <div className="logo">
+          <img
+            src="./images/logo-white.svg"
+            alt="company's logo"
+            className="footer__logo"
+          />
+        </div>
+        <p className="footer__text">copyright 2020. all rights reserved</p>
       </section>
     </section>
   );
