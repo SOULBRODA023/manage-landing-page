e269c9f7fd9f205a4ded2eeb623221bcf0f24e28
Soulbroda23 <ogundeyi.dieko13@gmail.com>
2023-10-17 01:52:42 -0600

    save my files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./component/Header";
 import FirstSection from "./component/FirstSection";
 import SecondSection from "./component/SecondSection";
 import Carousel from "./component/carousel";
-import Footer from "./component/Footer";
+import ThirdSection from "./component/Footer";
 import "src/style.css";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <FirstSection />
       <SecondSection />
       <Carousel />
-      <Footer />
+      <ThirdSection />
     </section>
   );
 }
